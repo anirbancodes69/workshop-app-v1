264a6b6d7ffdd37c620a0bb7da441c8a171208bd
Anirban <anirbancodes@gmail.com>
2025-04-03 23:51:24 +0530

    vercel config

--- server.js
+++ server.js
@@ -30,9 +30,9 @@ const writeItems = (items) => {
 
 // API Routes
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
 
 // GET all items
 app.get("/api/items", (req, res) => {
